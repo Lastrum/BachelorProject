9e5186e964e0a3a7338375a658cd7974e290aa73
Tanguy Geraedts <tanguy.geraedts4@gmail.com>
2022-04-17 22:52:59 +0200

    Update

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,30 @@
+using Manager;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        [SerializeField] private PlayerController playerController;
+
+        public bool GetLeftInput()
+        {
+            return Input.GetKeyDown(KeyCode.LeftArrow) || SwipeManager.swipeLeft;
+        }
+        
+        public bool GetRightInput()
+        {
+            return Input.GetKeyDown(KeyCode.RightArrow) || SwipeManager.swipeRight;
+        }
+        
+        public bool GetUpInput()
+        {
+            return Input.GetKeyDown(KeyCode.UpArrow) || SwipeManager.swipeUp;
+        }
+        
+        public bool GetDownInput()
+        {
+            return Input.GetKeyDown(KeyCode.DownArrow) || SwipeManager.swipeDown;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Manager;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+using static Player.PlayerController;
+using Debug = UnityEngine.Debug;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private PlayerController playerController;
+
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private LayerMask groundLayer;
+        private bool isGrounded;
+        
+        [Header("Movement")]
+        [SerializeField] public float forwardSpeed;
+        [SerializeField] public float jumpForce;
+        [SerializeField] public float gravity;
+        [SerializeField] public float slideLength;
+        
+        [NonSerialized] public Vector3 direction;
+        private Vector3 targetPosition;
+        private Vector3 velocity;
+
+        private bool jumped;
+        private bool sliding;
+
+        private void Awake()
+        {
+            jumped = false;
+            sliding = false;
+        }
+
+        private void Update()
+        {
+            if (playerController.currentBehaviour is PlayerController.Behaviour.Idle or PlayerController.Behaviour.Dead) return;
+            
+            //Moves Forward
+            direction.z = forwardSpeed;
+            direction.y += gravity * Time.deltaTime;
+
+
+            if (playerController.playerInput.GetUpInput())
+            {
+                playerController.currentSubBehaviour = SubBehaviour.Jumping;
+            }
+            
+            if (playerController.playerInput.GetDownInput())
+            {
+                playerController.currentSubBehaviour = SubBehaviour.Sliding;
+            }
+            
+            //Input for moving Left
+            if (playerController.playerInput.GetLeftInput())
+            {
+                SwitchLane(Vector2.left);
+            }
+            //Input for moving Right
+            if (playerController.playerInput.GetRightInput())
+            {
+                SwitchLane(Vector2.right);
+            }
+            
+            HandleLane();
+            
+            SubBehaviourChecker();
+            
+            transform.position = Vector3.Lerp(transform.position, targetPosition, playerController.transitionSpeed * Time.fixedDeltaTime);
+            playerController.controller.center = playerController.controller.center;
+        }
+
+        private void FixedUpdate()
+        {
+            if (playerController.currentBehaviour is PlayerController.Behaviour.Idle or PlayerController.Behaviour.Dead) return;
+            
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(groundCheck.transform.position, transform.TransformDirection(Vector3.down), out hit, 1, groundLayer))
+            {
+                //Debug.DrawRay(groundCheck.transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+
+            playerController.controller.Move(direction * Time.deltaTime);
+        }
+        
+        private void SwitchLane(Vector2 direction)
+        {
+            if (direction == Vector2.left)
+            {
+                playerController.currentLane = playerController.currentLane switch
+                {
+                    Lane.Right => Lane.Middle,
+                    Lane.Middle => Lane.Left,
+                    Lane.Left => Lane.Left,
+                };
+            }
+            if (direction == Vector2.right)
+            {
+                playerController.currentLane = playerController.currentLane switch
+                {
+                    Lane.Right => Lane.Right,
+                    Lane.Middle => Lane.Right,
+                    Lane.Left => Lane.Middle,
+                };
+            }
+        }
+
+        private void HandleLane()
+        {
+            //Calculate where we should be in the future
+            targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+            
+            switch (playerController.currentLane)
+            {
+                case Lane.Left:
+                    targetPosition += Vector3.left * playerController.laneDistance;
+                    break;
+                case Lane.Right:
+                    targetPosition += Vector3.right * playerController.laneDistance;
+                    break;
+            }
+        }
+
+        private void SubBehaviourChecker()
+        {
+            switch (playerController.currentSubBehaviour)
+            {
+                case SubBehaviour.Jumping:
+                    //Jump();
+                    break;
+                case SubBehaviour.Sliding:
+                    //Slide();
+                    break;
+            }
+        }
+
+        /*private async void Jump()
+        {
+            if (isGrounded && !jumped)
+            {
+                direction.y = jumpForce;
+                await Task.Delay(TimeSpan.FromSeconds(0.5f));
+                jumped = true;
+            }
+            if (!isGrounded || !jumped) return;
+            playerController.SetPlayerSubBehaviour(SubBehaviour.Nothing);
+            jumped = false;
+        }
+        
+        private async void Slide()
+        {
+            if (playerController.currentSubBehaviour == SubBehaviour.Jumping)
+            {
+                direction.y = gravity;
+            }
+            
+            await Task.Delay(TimeSpan.FromSeconds(slideLength));
+            playerController.SetPlayerSubBehaviour(SubBehaviour.Nothing);
+        }*/
+    }
+}
