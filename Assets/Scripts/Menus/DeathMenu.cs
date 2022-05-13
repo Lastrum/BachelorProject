771f2999442236d17e4a28f3b7191373b7cffce3
Lastrum <50644237+Lastrum@users.noreply.github.com>
2022-05-13 20:23:28 +0400

    Death-System Update Part I

--- Assets/Scripts/Menus/DeathMenu.cs
+++ Assets/Scripts/Menus/DeathMenu.cs
@@ -1,20 +1,34 @@
+using System;
+using Manager;
+using Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Menus
 {
     public class DeathMenu : MonoBehaviour
     {
+        [SerializeField] private PlayerController playerController;
+        [SerializeField] private Button respawnButton;
+        [SerializeField] private Button menuButton;
+
+        private void Awake()
+        {
+            respawnButton.onClick.AddListener(RespawnButton);
+            menuButton.onClick.AddListener(BackToMenuButton);
+        }
+
         public void RespawnButton()
         {
-            Debug.Log("Respawn");
-            SceneManager.LoadScene("Main");
+            playerController.playerMovement.forwardSpeed = playerController.playerMovement.speed;
+            MenuManager.SwitchMenu(MenuType.GameHud);
+            playerController.Respawning();
         }
 
         public void BackToMenuButton()
         {
-            Debug.Log("Quit");
-            //Back to Menu
+            SceneManager.LoadScene(0);
         }
     }
 }
